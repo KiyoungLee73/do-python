0119eadff9a6f0a91730c2d18f2a0bea7fb6d5dc
KiyoungLee73 <viracoch@naver.com>
2019-04-13 20:38:49 +0900

    python study

---
+++ else_test.py
@@ -0,0 +1,14 @@
+x1, x2, x3, x4 = map(int, input().split())
+
+if (x1 >= 0 and x1 <= 100
+    and x2 >= 0 and x2 <= 100
+    and x3 >= 0 and x3 <= 100
+    and x4 >= 0 and x4 <= 100
+    ):
+    avg_record = (x1+x2+x3+x4)/4
+    if (avg_record > 80):
+        print('합격')
+    else:
+        print('불합격')
+else:
+    print('잘못된 점수')
---
+++ if_test.py
@@ -0,0 +1,5 @@
+x = int(input())
+y = input()
+
+discount = int(y[4:len(y)])
+print(x-discount)
